74b33ced51283a52cd09b1f9c68324c0e462e6a8
naracanto <91744875+naracanto@users.noreply.github.com>
2022-03-17 21:50:56 +0100

    Add basic class 'TabularDocument'

---
+++ tabular_document.py
@@ -0,0 +1,27 @@
+# This Python file uses the following encoding: utf-8
+#
+# Copyright 2022 naracanto <https://naracanto.github.io>.
+#
+# This file is part of Tabelo-QtPy <https://github.com/tabelolabs/tabelo-qtpy>.
+#
+# Tabelo-QtPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tabelo-QtPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tabelo-QtPy.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from PySide2.QtWidgets import QWidget
+
+
+class TabularDocument(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
