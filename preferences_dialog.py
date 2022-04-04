0e3b6885de90a2aaa4e54849ec9983fe2d8f4a32
naracanto <91744875+naracanto@users.noreply.github.com>
2022-04-04 15:12:48 +0200

    Rename application

--- preferences_dialog.py
+++ preferences_dialog.py
@@ -2,20 +2,20 @@
 #
 # Copyright 2022 naracanto <https://naracanto.github.io>.
 #
-# This file is part of Tabelo-QtPy <https://github.com/cutelabs/tabelo-qtpy>.
+# This file is part of PyTabelo <https://github.com/cutelabs/pytabelo>.
 #
-# Tabelo-QtPy is free software: you can redistribute it and/or modify
+# PyTabelo is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Tabelo-QtPy is distributed in the hope that it will be useful,
+# PyTabelo is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Tabelo-QtPy.  If not, see <https://www.gnu.org/licenses/>.
+# along with PyTabelo.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 from PySide2.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout
